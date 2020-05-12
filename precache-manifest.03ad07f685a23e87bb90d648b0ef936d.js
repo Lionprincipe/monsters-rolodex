f5f28ae5bce86d7623cdee0d598a4aca4354c1fc
Lionel <lionprincipe@hotmail.com>
2020-05-12 21:39:46 +0200

    Updates

---
+++ precache-manifest.03ad07f685a23e87bb90d648b0ef936d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69242f6b9e9f7fda994d5100afb5173f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "119496d8d23993b26ebe",
+    "url": "/monsters-rolodex/static/css/main.f88597d5.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119496d8d23993b26ebe",
+    "url": "/monsters-rolodex/static/js/main.ad809387.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
